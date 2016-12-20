e3e9fa7c662a3322c785953e69e45abb53f76ea9
Sherry <shuyao1201@gmail.com>
2016-12-19 21:51:39 -0500

    update timeaxis

--- projects/final_project/js/timeaxis.js
+++ projects/final_project/js/timeaxis.js
@@ -1,15 +1,15 @@
 //画坐标轴
 	function drawAxisTime(filename, svg, w, h, data) {
-		
+
 		//var data = ["1995", "1996", "1997"];
 		var padding = 60;
 		var axisw  = w - 2*padding;
 		var axish = h;
-		
-		var svgTimeAxis = svg.append("g")//在body的结尾添加svg
-			    .attr("class", "axis") // 指定"axis" 类;
-					
-		//画坐标线<line x1="0" y1="0" x2="500" y2="50" stroke="black"/>
+
+		var svgTimeAxis = svg.append("g")
+			    .attr("class", "axis")
+
+
 		svgTimeAxis.append("line")
 				   .attr("x1", padding)
 				   .attr("y1", axish)
@@ -17,16 +17,16 @@
 				   .attr("y2", axish)
 				   .attr("stroke", "black");
 
-		//设置坐标轴的比例尺
+
 		var length = data.length;
-		//序数坐标比例尺
+
 		var xScale = d3.scale.linear()
 				.domain([0, length-1])
 				.range([padding, axisw+padding]);
 
 		var r = 5;
 
-		//画原点
+
 		var circles = svgTimeAxis.selectAll("circle")
 			.data(data)
 			.enter()
@@ -44,12 +44,12 @@
 			})
 			.on("click", function(d){
 				//var year = this.id;
-				circlemouseover(filename, provinceId, coutiesId, d, this);	
+				circlemouseover(filename, provinceId, coutiesId, d, this);
 			})
 			.attr("cy", axish)
 			.attr("r", r);
 
-		//画坐标值 <text x="250" y="25">Easy-peasy</text>
+
 		var texts = svgTimeAxis.selectAll("text")
 			.data(data)
 			.enter()
@@ -61,56 +61,55 @@
 				return xScale(i);
 			})
 			.attr("y", axish+30)
-			.attr("text-anchor", "middle");//文本居中
+			.attr("text-anchor", "middle");
 	}
 
-	//点击原点轴原点时间
+
 	function circlemouseover(filename, provinceId, coutiesId, year, t){
 
 		globalyear = year;
 
-		//现将所有的圆的坐标回复成原大小
 		d3.selectAll(".circle")
 		  .attr("r", 5)
 		  .style("fill", "#63B8FF")
-		//一下代码是将坐标轴上的圆变大
+
 		if(t != undefined) {
 			var circle = d3.select(t);
 			circle.transition()
 	  			.duration(750)
-	  			.attr("r", function(d){  //设置圆点半径                      
-						return 5+5;                          
+	  			.attr("r", function(d){
+						return 5+5;
 					}).style("fill", "#F05462");
 	  	}
-  		
-  		//var jsonpath = "json/" +this.id + ".json";
+
+
   		var jsonpath = "json/data/" + filename + "/" + year + ".json";
 
-  		//画国家地图上的饼图
+
   		d3.json(jsonpath, function(error, yeardata){
-  			
-				if (error)  
+
+				if (error)
 					return console.error(error);
 				if(yeardata == undefined){
 					return ;
 					}
-  			
-			//d3.selectAll("#pie").transition().duration(350).remove();//删除原来的pie
+
+			//d3.selectAll("#pie").transition().duration(350).remove();
 			d3.selectAll("#pie").remove();
-			
-			d3.selectAll(".province_name").remove();//删除原来的省名
-			d3.selectAll(".city_name").remove();//删除原来的市名
-			d3.selectAll(".county_name").remove();//删除原来的县名
-			//比例尺函数
+
+			d3.selectAll(".province_name").remove();
+			d3.selectAll(".city_name").remove();
+			d3.selectAll(".county_name").remove();
+
 	  		var scale = d3.scale.linear()
 			.domain([yeardata.min, yeardata.max])
 			.range([15, 35])
 			.nice();
-			
-			
-			
+
+
+
 			provinceDataYear=yeardata;  //记录下每个省的数据，为背景色准备
-			
+
 			var provinceMax=yeardata.max;
 			var provinceMin=yeardata.min;
 			var provinceData = [];
@@ -119,7 +118,7 @@
 				provinceData.push(yeardata[provinceDataKey[i]].sum);
 			}
 			drawRangeFinal(provinceMax, provinceMin, provinceData);   //画范围控制条
-			
+
 			//给国家地图中的省加饼图
 			nodes.forEach(function(d){
 				var name = yeardata[d.id];
@@ -141,16 +140,16 @@
 			var jsonpath = "json/data/" + provinceId + "_" + year + ".json";
 			d3.json(jsonpath, function(error, yeardata){
 				//比例尺函数
-				
+
 					if(yeardata == undefined){
 					return ;
 					}
-					
+
 		  	var scale = d3.scale.linear()
 				.domain([yeardata.min, yeardata.max])
 				.range([15, 35])
 				.nice();
-				
+
 				provinceNodes.forEach(function(d){
 					var name = yeardata[d.id];
 					if(name != undefined) {
@@ -174,13 +173,13 @@
 				if(yeardata == undefined){
 					return ;
 					}
-			
+
 				//比例尺函数
 		  		var scale = d3.scale.linear()
 				.domain([yeardata.min, yeardata.max])
 				.range([15, 35])
 				.nice();
-				
+
 				coutiesNodes.forEach(function(d){
 					var name = yeardata[d.id];
 					if(name != undefined) {
@@ -196,7 +195,7 @@
 				});
 			});
 		}
-		
-		
-	
+
+
+
 	}
